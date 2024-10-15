e082a2a5d7e5670dae5d7ec176a7358a4f17dd13
Denis <dp1505ar@gmail.com>
2024-10-15 19:03:30 +0300

    рабочий тест

--- tests/models.py
+++ tests/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from main.models import Article, Course
+from users.models import Profile
 
+
+    
 # Тест для статьи
 class Test(models.Model):
     article = models.OneToOneField(Article, on_delete=models.CASCADE, related_name='tests')
     title = models.CharField(max_length=255)
-    prerequisite_test = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'Test for {self.article.title}'
@@ -22,42 +24,35 @@ class Question(models.Model):
     CHOICE = 'choice'
 
     QUESTION_TYPES = [
-        (OPEN, 'Открытый вопрос'),
-        (CHOICE, 'Множественный выбор'),
+        (OPEN, 'Open-ended'),
+        (CHOICE, 'Multiple choice'),
     ]
 
     test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='questions')
-    text = models.TextField()
-    question_type = models.CharField(max_length=10, choices=QUESTION_TYPES, default=CHOICE)
-    correct_answer = models.TextField()
-
+    text_description = models.TextField() #само задание
+    text_task = models.TextField() #содержание задания
+    question_type = models.CharField(max_length=17, choices=[('multiple_choice', 'Multiple Choice'), ('text', 'Text')])
+    
     def __str__(self):
-        return f'{self.text}'
+        return f'{self.text_task}'
 
     class Meta:
         verbose_name = 'Вопрос'
         verbose_name_plural = 'Вопросы'
 
 
-
-
-
-
-# # Вариант ответа
+# Вариант ответа
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, limit_choices_to={'question_type': 'choice'})
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     is_correct = models.BooleanField(default=False)
     
     def __str__(self):
-        return self.text
+        return  f'{self.text}'
 
     class Meta:
         verbose_name = 'Ответ'
         verbose_name_plural = 'Ответы'
-
-
-
 
 # Открытые ответы пользователя (для вопросов с открытым ответом)
 class OpenAnswer(models.Model):
@@ -66,33 +61,22 @@ class OpenAnswer(models.Model):
     answer_text = models.TextField()
 
     def __str__(self):
-        return f'Answer by {self.user.username} for question {self.question.text}'
+        return f'Answer by {self.user.username} for question {self.question}'
+    
+    # def __str__(self):
+    #     return f'Answer by {self.user.username} for question {self.question.text}'
 
     class Meta:
         verbose_name = 'Открытый ответ'
         verbose_name_plural = 'Открытые ответы'
 
 
-# Результаты теста пользователя
-class TestResult(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
-    passed = models.BooleanField(default=False)
-    score = models.FloatField(default=0.0)
-    attempt_count = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return f'{self.user.username} - {self.test.title} - {"Passed" if self.passed else "Failed"}'
-
-    class Meta:
-        verbose_name = 'Результат теста'
-        verbose_name_plural = 'Результаты тестов'
 
 
 # Финальный тест по курсу
 class FinalTest(models.Model):
     course = models.OneToOneField(Course, on_delete=models.CASCADE)
-    questions = models.ManyToManyField(Question)
+    questions = models.ManyToManyField(Question)  # Вопросы финального теста
 
     def __str__(self):
         return f"Final Test for {self.course.title}"
@@ -118,6 +102,29 @@ class FinalTestResult(models.Model):
         verbose_name_plural = 'Результаты финальных тестов'
 
 
+
+
+
+class TestResult(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    score = models.IntegerField()  # Количество правильных ответов
+    total_questions = models.IntegerField()  # Общее количество вопросов
+    passed = models.BooleanField(default=False)
+
+    answer_text = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.profile.user.username} - {self.test.title} - {self.score}"
+
+    class Meta:
+        verbose_name = 'Результат теста'
+        verbose_name_plural = 'Результаты тестов'
+
+
+
+
+
 # Модель для связи теста и курса (или статьи)
 class Enrollment(models.Model):
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
@@ -126,6 +133,4 @@ class Enrollment(models.Model):
 
     def __str__(self):
         return f"{self.test.title} тест от  {self.article.title}"
-
-
 
--- tests/views.py
+++ tests/views.py
@@ -1,77 +1,135 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from .models import Test, Question, TestResult, FinalTest, Answer, Course, Article
+from main.models import Course, Article
+from users.models import Profile
+from .models import Test, Answer, TestResult, FinalTest, OpenAnswer
+from users.models import Enrollment
+from django.contrib.auth.decorators import login_required
 
 
-
-def test_detail(request, course_id, article_id, test_id):
-    # Получаем курс, статью и тест
-    course = get_object_or_404(Course, id=course_id)
-    article = get_object_or_404(Article, id=article_id)
-    test = get_object_or_404(Test, id=test_id)
-    questions = test.questions.all()
-
-    # Проверим, доступен ли тест (если есть предыдущий тест)
-    if test.prerequisite_test and not TestResult.objects.filter(test=test.prerequisite_test, user=request.user, passed=True).exists():
-        return redirect('test_locked')
+def test_detail_ok(request, course_id, article_id, test_id):
+    # test = Test.objects.get(id=article_id)
+    test = get_object_or_404(Test, id=article_id)
+    course = Test.objects.filter(id=course_id) 
+    # article = get_object_or_404(Article, id=article_id, course=course)
+    questions = test.questions.all().prefetch_related('answers')
 
     context = {
         'course': course,
-        'article': article,
+        # 'article': article,
         'test': test,
-        'questions': questions,
+        'questions': questions
     }
     return render(request, 'tests/test_detail.html', context)
 
 
 
 
-# Прохождение теста
-def pass_test(request, test_id):
-    test = get_object_or_404(Test, id=test_id)
+
+@login_required
+def test_detail(request, course_id, article_id, test_id):
+    print(f"Querying Profile with course_id={course_id}, article_id={article_id}, test_id={test_id}")
+    course = get_object_or_404(Course, id=course_id)
+    article = get_object_or_404(Article, id=article_id, course=course)
+    test = get_object_or_404(Test, id=test_id, article=article)
+    profile = get_object_or_404(Profile, user=request.user)
     questions = test.questions.all()
-    user_answers = request.POST  # Получаем ответы пользователя из формы
 
-    score = 0
-    for question in questions:
-        if question.question_type == Question.CHOICE or question.question_type == Question.MULTIPLE_CHOICE:
-            selected_answer = user_answers.get(f'question_{question.id}')
-            if selected_answer and Answer.objects.get(id=selected_answer).is_correct:
-                score += 1
-        elif question.question_type == Question.OPEN:
-            # Логика проверки открытых ответов
-            pass
+    if request.method == 'POST':
+        score = 0
+        total = questions.count()
+        # Создаем список для хранения текстовых ответов
+        text_answers = {}
 
-    passed = score >= len(questions) * 0.7  # Условие прохождения
-    result = TestResult.objects.create(user=request.user, test=test, score=score, passed=passed)
+        for question in questions:
+            field_name = f'question_{question.id}'
+            user_answer = request.POST.get(field_name)
 
-    # Обновляем лучшую попытку
-    TestResult.objects.filter(user=request.user, test=test).update(best_attempt=False)
-    result.best_attempt = True
-    result.save()
+            if question.question_type == 'multiple_choice':
+                try:
+                    selected_answer = Answer.objects.get(id=user_answer, question=question)
+                    if selected_answer.is_correct:
+                        score += 1
+                except Answer.DoesNotExist:
+                    pass  # Неверный выбор или не выбран ответ
+
+            elif question.question_type == 'text':
+                # Сохраняем текстовый ответ
+                text_answers[question.id] = user_answer.strip()  # Убираем пробелы в начале и конце
+
+        # Здесь вы можете обрабатывать текстовые ответы для хранения
+        for question_id, user_answer in text_answers.items():
+            # Вы можете создать отдельную модель для хранения текстовых ответов или
+            # просто добавить текст ответа в TestResult
+            TestResult.objects.create(
+                profile=profile,
+                test=test,
+                score=score,
+                total_questions=total,
+                passed=(score >= (total - 1)),
+                answer_text=user_answer  # Сохраняем текст ответа
+            )
+
+        return redirect('test_results', course_id=course.id, article_id=article.id, test_id=test.id)
+
+    return render(request, 'tests/test_detail.html', {
+        'course': course,
+        'article': article,
+        'test': test,
+        'questions': questions,
+    })
+
+
+
+
+@login_required
+def test_results(request, course_id, article_id, test_id):
+    course = get_object_or_404(Course, id=course_id)
+    article = get_object_or_404(Article, id=article_id, course=course)
+    test = get_object_or_404(Test, id=test_id, article=article)
+    # profile = get_object_or_404(Profile, user=request.user)
+
+    profile, created = Profile.objects.get_or_create(user=request.user)
+
+    # Получаем последний результат теста пользователя
+    # result = TestResult.objects.filter(profile=profile, test=test).order_by('-date_taken').first()
+
+    result = TestResult.objects.filter(profile=profile, test=test).first()
 
     context = {
+        'course': course,
+        'article': article,
         'test': test,
-        'score': score,
-        'passed': passed,
-        'best_attempt': result
+        'result': result,
     }
-    return render(request, 'tests/test_result.html', context)
+
+    return render(request, 'tests/test_results.html', context)
+
+# Прохождение теста
+class TestPassView(View):
+    pass
+    # def post(self, request, course_id, article_id, test_id):
+    #     test = get_object_or_404(Test, id=test_id, article_id=article_id)
+    #     # Логика прохождения теста
+    #     return render(request, 'main/test_result.html', {'test': test})
+
 
 # Детали финального теста
 class FinalTestDetailView(View):
-    def get(self, request, course_id, test_id):
-        final_test = get_object_or_404(FinalTest, id=test_id)
-        # Проверяем, все ли предыдущие тесты пройдены
-        for test in final_test.prerequisite_tests.all():
-            if not TestResult.objects.filter(test=test, user=request.user, passed=True).exists():
-                return redirect('test_locked')
+    pass
+    # def get(self, request, course_id, test_id):
+    #     final_test = get_object_or_404(FinalTest, id=test_id, course_id=course_id)
+    #     return render(request, 'main/final_test_detail.html', {'final_test': final_test})
 
-        return render(request, 'tests/final_test_detail.html', {'final_test': final_test})
 
 # Прохождение финального теста
 class FinalTestPassView(View):
-    def post(self, request, course_id, test_id):
-        final_test = get_object_or_404(FinalTest, id=test_id)
-        # Логика прохождения финального теста (аналогично обычным тестам)
-        pass
+    pass
+    # def post(self, request, course_id, test_id):
+    #     final_test = get_object_or_404(FinalTest, id=test_id, course_id=course_id)
+    #     # Логика прохождения финального теста
+    #     return render(request, 'main/final_test_result.html', {'final_test': final_test})
+
+
+
+
