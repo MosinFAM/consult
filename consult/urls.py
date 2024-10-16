2c8530c723cd1d54d85ba982c6add360a05b7f52
MosinFAM <alexfilinxxx@mail.ru>
2024-10-16 17:42:36 +0300

    Добавил строку поиска

--- consult/urls.py
+++ consult/urls.py
@@ -24,7 +24,6 @@ from users.views import logout_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    # path('search/', main_views.search_courses, name='search_courses'),  # URL для поиска
     path('tests/', include('tests.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
